30a430346b7011796134b0c23da1f4eec0d34a24
weiyichong <864147974@github.com>
2023-02-04 03:22:14 +0800

    demo1

---
+++ code/play.py
@@ -0,0 +1,169 @@
+import os, argparse
+
+import cv2
+
+import torch
+from torch.autograd import Variable
+from torchvision import transforms
+import torch.backends.cudnn as cudnn
+import torchvision
+import torch.nn.functional as F
+from PIL import Image
+
+import hopenet, utils
+
+from mark_detector import MarkDetector
+
+# python code/play.py --snapshot ./model/hopenet_robust_alpha1.pkl --face_model ./mmod_human_face_detector.dat --video 0
+
+
+def parse_args():
+    """Parse input arguments."""
+    parser = argparse.ArgumentParser(description='Head pose estimation using the Hopenet network.')
+    parser.add_argument('--gpu', dest='gpu_id', help='GPU device id to use [0]',
+            default=0, type=int)
+    parser.add_argument('--snapshot', dest='snapshot', help='Path of model snapshot.',
+          default='', type=str)
+    parser.add_argument('--face_model', dest='face_model', help='Path of DLIB face detection model.',
+          default='', type=str)
+    parser.add_argument('--video', dest='video_path', help='Path of video')
+    parser.add_argument('--output_string', dest='output_string', help='String appended to output file')
+    parser.add_argument('--n_frames', dest='n_frames', help='Number of frames', type=int)
+    parser.add_argument('--fps', dest='fps', help='Frames per second of source video', type=float, default=30.)
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    cudnn.enabled = True
+
+    batch_size = 1
+    gpu = args.gpu_id
+    snapshot_path = args.snapshot
+    out_dir = 'output/video'
+    video_path = args.video_path
+
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    if not os.path.exists(args.video_path):
+        # sys.exit('Video does not exist')
+        video_path = 0 # use camera
+
+    # ResNet50 structure
+    model = hopenet.Hopenet(torchvision.models.resnet.Bottleneck, [3, 4, 6, 3], 66)
+
+    # Dlib face detection model
+    # cnn_face_detector = dlib.cnn_face_detection_model_v1(args.face_model)
+    # cnn_face_detector = dlib.get_frontal_face_detector()
+    mark_detector = MarkDetector()
+
+    print('Loading snapshot.')
+    # Load snapshot
+    saved_state_dict = torch.load(snapshot_path)
+    model.load_state_dict(saved_state_dict)
+
+    print('Loading data.')
+
+    transformations = transforms.Compose([transforms.Resize(224),
+    transforms.CenterCrop(224), transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+
+    gpu = 0
+    model.cuda(gpu)
+
+    print('Ready to test network.')
+
+    # Test the Model
+    model.eval()  # Change model to 'eval' mode (BN uses moving mean/var).
+    total = 0
+
+    idx_tensor = [idx for idx in range(66)]
+    idx_tensor = torch.FloatTensor(idx_tensor).cuda(gpu)
+
+    video = cv2.VideoCapture(video_path)
+
+    # New cv2
+    width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))   # float
+    height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT)) # float
+
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+    out = cv2.VideoWriter('output/video/output-%s.avi' % args.output_string, fourcc, args.fps, (width, height))
+
+    # # Old cv2
+    # width = int(video.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))   # float
+    # height = int(video.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)) # float
+    #
+    # # Define the codec and create VideoWriter object
+    # fourcc = cv2.cv.CV_FOURCC(*'MJPG')
+    # out = cv2.VideoWriter('output/video/output-%s.avi' % args.output_string, fourcc, 30.0, (width, height))
+
+    txt_out = open('output/video/output-%s.txt' % args.output_string, 'w')
+
+    frame_num = 1
+
+    tm = cv2.TickMeter()
+
+    while True:
+        print(frame_num)
+
+        ret, frame = video.read()
+        if not ret:
+            break
+
+        cv2_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # if video_path==0:
+        #     cv2_frame = cv2.flip(cv2_frame, 2)
+
+        # Dlib detect
+
+        tm.start()
+        facebox = mark_detector.extract_cnn_facebox(frame)
+        tm.stop()
+
+
+        if facebox is not None:
+
+            x_min, y_min, x_max, y_max = facebox
+            print(x_min, x_max, y_min, y_max)
+            bbox_width = abs(x_max - x_min)
+            bbox_height = abs(y_max - y_min)
+
+            # Crop image
+            img = cv2_frame[int(y_min):int(y_max),int(x_min):int(x_max)]
+            img = Image.fromarray(img)
+
+            # Transform
+            img = transformations(img)
+            img_shape = img.size()
+            img = img.view(1, img_shape[0], img_shape[1], img_shape[2])
+            img = Variable(img).cuda(gpu)
+
+            yaw, pitch, roll = model(img)
+
+            yaw_predicted = F.softmax(yaw)
+            pitch_predicted = F.softmax(pitch)
+            roll_predicted = F.softmax(roll)
+            # Get continuous predictions in degrees.
+            yaw_predicted = torch.sum(yaw_predicted.data[0] * idx_tensor) * 3 - 99
+            pitch_predicted = torch.sum(pitch_predicted.data[0] * idx_tensor) * 3 - 99
+            roll_predicted = torch.sum(roll_predicted.data[0] * idx_tensor) * 3 - 99
+
+            # Print new frame with cube and axis
+            txt_out.write(str(frame_num) + ' %f %f %f\n' % (yaw_predicted, pitch_predicted, roll_predicted))
+            # utils.plot_pose_cube(frame, yaw_predicted, pitch_predicted, roll_predicted, (x_min + x_max) / 2, (y_min + y_max) / 2, size = bbox_width)
+            utils.draw_axis(frame, yaw_predicted, pitch_predicted, roll_predicted, tdx = (x_min + x_max) / 2, tdy= (y_min + y_max) / 2, size = bbox_height/2)
+            # Plot expanded bounding box
+            # cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0,255,0), 1)
+
+        out.write(frame)
+        cv2.imshow("Preview", frame)
+        if cv2.waitKey(1) == 27:
+            break
+        frame_num += 1
+
+    out.release()
+    video.release()
--- code/test_on_video_dlib.py
+++ code/test_on_video_dlib.py
@@ -1,23 +1,19 @@
-import sys, os, argparse
+import os, argparse
 
-import numpy as np
 import cv2
-import matplotlib.pyplot as plt
 
 import torch
-import torch.nn as nn
 from torch.autograd import Variable
-from torch.utils.data import DataLoader
 from torchvision import transforms
 import torch.backends.cudnn as cudnn
 import torchvision
 import torch.nn.functional as F
 from PIL import Image
 
-import datasets, hopenet, utils
+import hopenet, utils
 
-from skimage import io
-import dlib
+from mark_detector import MarkDetector
+
 
 def parse_args():
     """Parse input arguments."""
@@ -50,34 +46,38 @@ if __name__ == '__main__':
         os.makedirs(out_dir)
 
     if not os.path.exists(args.video_path):
-        sys.exit('Video does not exist')
+        # sys.exit('Video does not exist')
+        video_path = 0 # use camera
 
     # ResNet50 structure
     model = hopenet.Hopenet(torchvision.models.resnet.Bottleneck, [3, 4, 6, 3], 66)
 
     # Dlib face detection model
-    cnn_face_detector = dlib.cnn_face_detection_model_v1(args.face_model)
+    # cnn_face_detector = dlib.cnn_face_detection_model_v1(args.face_model)
+    # cnn_face_detector = dlib.get_frontal_face_detector()
+    mark_detector = MarkDetector()
 
-    print 'Loading snapshot.'
+    print('Loading snapshot.')
     # Load snapshot
     saved_state_dict = torch.load(snapshot_path)
     model.load_state_dict(saved_state_dict)
 
-    print 'Loading data.'
+    print('Loading data.')
 
-    transformations = transforms.Compose([transforms.Scale(224),
+    transformations = transforms.Compose([transforms.Resize(224),
     transforms.CenterCrop(224), transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
+    gpu = 0
     model.cuda(gpu)
 
-    print 'Ready to test network.'
+    print('Ready to test network.')
 
     # Test the Model
     model.eval()  # Change model to 'eval' mode (BN uses moving mean/var).
     total = 0
 
-    idx_tensor = [idx for idx in xrange(66)]
+    idx_tensor = [idx for idx in range(66)]
     idx_tensor = torch.FloatTensor(idx_tensor).cuda(gpu)
 
     video = cv2.VideoCapture(video_path)
@@ -102,63 +102,65 @@ if __name__ == '__main__':
 
     frame_num = 1
 
-    while frame_num <= args.n_frames:
-        print frame_num
+    tm = cv2.TickMeter()
 
-        ret,frame = video.read()
-        if ret == False:
+    while True:
+        print(frame_num)
+
+        ret, frame = video.read()
+        if not ret:
             break
 
-        cv2_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        cv2_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # if video_path==0:
+        #     cv2_frame = cv2.flip(cv2_frame, 2)
 
         # Dlib detect
-        dets = cnn_face_detector(cv2_frame, 1)
 
-        for idx, det in enumerate(dets):
-            # Get x_min, y_min, x_max, y_max, conf
-            x_min = det.rect.left()
-            y_min = det.rect.top()
-            x_max = det.rect.right()
-            y_max = det.rect.bottom()
-            conf = det.confidence
+        tm.start()
+        facebox = mark_detector.extract_cnn_facebox(frame)
+        tm.stop()
 
-            if conf > 1.0:
-                bbox_width = abs(x_max - x_min)
-                bbox_height = abs(y_max - y_min)
-                x_min -= 2 * bbox_width / 4
-                x_max += 2 * bbox_width / 4
-                y_min -= 3 * bbox_height / 4
-                y_max += bbox_height / 4
-                x_min = max(x_min, 0); y_min = max(y_min, 0)
-                x_max = min(frame.shape[1], x_max); y_max = min(frame.shape[0], y_max)
-                # Crop image
-                img = cv2_frame[y_min:y_max,x_min:x_max]
-                img = Image.fromarray(img)
 
-                # Transform
-                img = transformations(img)
-                img_shape = img.size()
-                img = img.view(1, img_shape[0], img_shape[1], img_shape[2])
-                img = Variable(img).cuda(gpu)
+        if facebox is not None:
 
-                yaw, pitch, roll = model(img)
+            x_min, y_min, x_max, y_max = facebox
+            print(x_min, x_max, y_min, y_max)
+            bbox_width = abs(x_max - x_min)
+            bbox_height = abs(y_max - y_min)
 
-                yaw_predicted = F.softmax(yaw)
-                pitch_predicted = F.softmax(pitch)
-                roll_predicted = F.softmax(roll)
-                # Get continuous predictions in degrees.
-                yaw_predicted = torch.sum(yaw_predicted.data[0] * idx_tensor) * 3 - 99
-                pitch_predicted = torch.sum(pitch_predicted.data[0] * idx_tensor) * 3 - 99
-                roll_predicted = torch.sum(roll_predicted.data[0] * idx_tensor) * 3 - 99
+            # Crop image
+            img = cv2_frame[int(y_min):int(y_max),int(x_min):int(x_max)]
+            img = Image.fromarray(img)
 
-                # Print new frame with cube and axis
-                txt_out.write(str(frame_num) + ' %f %f %f\n' % (yaw_predicted, pitch_predicted, roll_predicted))
-                # utils.plot_pose_cube(frame, yaw_predicted, pitch_predicted, roll_predicted, (x_min + x_max) / 2, (y_min + y_max) / 2, size = bbox_width)
-                utils.draw_axis(frame, yaw_predicted, pitch_predicted, roll_predicted, tdx = (x_min + x_max) / 2, tdy= (y_min + y_max) / 2, size = bbox_height/2)
-                # Plot expanded bounding box
-                # cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0,255,0), 1)
+            # Transform
+            img = transformations(img)
+            img_shape = img.size()
+            img = img.view(1, img_shape[0], img_shape[1], img_shape[2])
+            img = Variable(img).cuda(gpu)
+
+            yaw, pitch, roll = model(img)
+
+            yaw_predicted = F.softmax(yaw)
+            pitch_predicted = F.softmax(pitch)
+            roll_predicted = F.softmax(roll)
+            # Get continuous predictions in degrees.
+            yaw_predicted = torch.sum(yaw_predicted.data[0] * idx_tensor) * 3 - 99
+            pitch_predicted = torch.sum(pitch_predicted.data[0] * idx_tensor) * 3 - 99
+            roll_predicted = torch.sum(roll_predicted.data[0] * idx_tensor) * 3 - 99
+
+            # Print new frame with cube and axis
+            txt_out.write(str(frame_num) + ' %f %f %f\n' % (yaw_predicted, pitch_predicted, roll_predicted))
+            # utils.plot_pose_cube(frame, yaw_predicted, pitch_predicted, roll_predicted, (x_min + x_max) / 2, (y_min + y_max) / 2, size = bbox_width)
+            utils.draw_axis(frame, yaw_predicted, pitch_predicted, roll_predicted, tdx = (x_min + x_max) / 2, tdy= (y_min + y_max) / 2, size = bbox_height/2)
+            # Plot expanded bounding box
+            # cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0,255,0), 1)
 
         out.write(frame)
+        cv2.imshow("Preview", frame)
+        if cv2.waitKey(1) == 27:
+            break
         frame_num += 1
 
     out.release()
